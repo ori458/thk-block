4807eb3d85def6aa88a1b0b05062119dde8fa9e2
ori458 <94099307+ori458@users.noreply.github.com>
2022-07-28 08:30:27 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.pause(1000)
+kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 0)
+kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, 0)
+kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, 0)
+basic.forever(function () {
+	
+})
